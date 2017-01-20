00fc736564d6f1d16e5a878261bf3a5f24671aa0
Stephen Parker <shurakaisoft@gmail.com>
2017-01-20 23:52:30 +1000

    added notes on creating a migration script

--- example/migrations/0.2.1.js
+++ example/migrations/0.2.1.js
@@ -1,6 +1,16 @@
 /**
  * An example migration script for testing
  *
+ *  1. Migration files are named to match the version of your application as defined in your package.json file.
+ *  2. Migration files export an object containing two functions: 'migrate' and 'rollback'.
+ *  3. The migrate script runs when the target version is <= this version.
+ *  4. The rollback script runs when the rollback target is < this version
+ *  5. If the migrate script fails, it is responsible for rewinding any partially applied migrations, so the state of the
+ *     system is as if the migration never ran.
+ *  6. If a rollback fails.... ????? need to crash with some logging details, a human will most likely be required to
+ *     sort out this messy state....
+ *  7. Scripts when successful, should return a resolved promise.
+ *  8. If a script fails, after undoing stuff it should return a rejected promise.
  *
  */
 'use strict'
